58ff24e2035152ba23e62c7539455be3d46d1e6c
Gabriel <91475996+bdshrk@users.noreply.github.com>
2022-11-29 20:09:29 +0000

    basic fsm

--- Assets/Scripts/PathFind.cs
+++ Assets/Scripts/PathFind.cs
@@ -7,21 +7,66 @@ public class PathFind : MonoBehaviour
 {
     public Vector3 goal;
     private GameObject player;
-    private Light light;
+    private Light spotlight;
 
+    private Vector3 startPos;
     public float wanderDistMax = 250.0f;
-    public float wanderDistMin = 150.0f;
-    public float distToStop = 2.5f;
+    public float timeRangeMin = 15.0f;
+    public float timeRangeMax = 30.0f;
+    private float timeCountdown;
+
+
+    public float angleToSeePlayer = 25.0f;
+    public float distanceToSeePlayer = 15.0f;
+    private State currentState;
+
+    private enum State
+    {
+        WANDER,
+        CHASE
+    }
+
 
     private void Start()
     {
         player = GameObject.FindGameObjectsWithTag("Player")[0];
+        startPos = transform.position;
+        spotlight = this.transform.Find("Light").GetComponent<Light>();
+        currentState = State.WANDER;
+
         newGoal();
-        light = this.transform.Find("Light").GetComponent<Light>();
     }
 
     void Update()
     {
+        switch (currentState)
+        {
+            case State.WANDER:
+                wander();
+                break;
+            case State.CHASE:
+                chase();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void chase() {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+
+        timeCountdown = Random.Range(timeRangeMin, timeRangeMax);
+        goal = player.transform.position;
+        agent.destination = goal;
+
+        if (!raycastHitPlayer()) {
+            currentState = State.WANDER;
+        }
+
+        spotlight.color = Color.red;
+    }
+
+    private void wander() {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = goal;
 
@@ -30,48 +75,50 @@ public class PathFind : MonoBehaviour
         Vector2 vec2Us = new Vector2(transform.position.x, transform.position.z);
         Vector2 vec2Forward = new Vector2(transform.forward.x, transform.forward.z);
 
-        print(agent.pathStatus);
-        print(agent.remainingDistance);
+        // print(agent.pathStatus);
+        // print(agent.remainingDistance);
 
-        if (agent.remainingDistance <= distToStop)
-        {
+        timeCountdown -= Time.deltaTime;
+        if (timeCountdown <= 0) {
             newGoal();
         }
 
-       // if (agent.pathStatus == NavMeshPathStatus.Path)
-       // {
-       //     newGoal();
-       // }
-
         Vector2 playerDirection = vec2Player - vec2Us;
         float angle = Vector2.Angle(playerDirection, vec2Forward);
+        float distance = Vector2.Distance(vec2Player, vec2Us);
 
-        if (angle <= 25.0f)
+        if (angle < angleToSeePlayer && distance < distanceToSeePlayer)
         {
-            light.color = Color.red;
+            // Raycast is expensive, so check angle and distance first.
+            if (raycastHitPlayer()) {
+                currentState = State.CHASE;
+            }
         }
-        else
-        {
-            light.color = Color.white;
-        }
+
+        spotlight.color = Color.white;
     }
 
-    private Vector2 randomPointInRadius()
-    {
-        return new Vector2(Random.Range(wanderDistMin, wanderDistMax), Random.Range(wanderDistMin, wanderDistMax));
+    private bool raycastHitPlayer() {
+        LayerMask layers = ~(1 << gameObject.layer);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, distanceToSeePlayer + 1.0f, layers)) {
+            if (hit.collider.gameObject.tag == "Player") {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void newGoal()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        NavMeshHit valid;
 
-        Vector2 circle = Random.insideUnitCircle.normalized + randomPointInRadius();
+        NavMesh.SamplePosition(startPos + (Random.insideUnitSphere * wanderDistMax), out valid, wanderDistMax, -1);
 
-        goal = new Vector3(
-            transform.position.x + circle.x,
-            transform.position.y,
-            transform.position.z + circle.y
-        );
-        
+        goal = valid.position;
+
+        timeCountdown = Random.Range(timeRangeMin, timeRangeMax);
     }
 }
